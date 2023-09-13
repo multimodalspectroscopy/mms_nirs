0f44cedd1054ab6d4fda7fe43eb0ba6a941d28de
Josh Buckland <hello@itsjoshb.com>
2023-09-13 11:05:01 +0100

    Add docstrings

--- mms_nirs/utils/dpf.py
+++ mms_nirs/utils/dpf.py
@@ -9,6 +9,17 @@ def calc_mua(
     hbo2: np.ndarray,
     extinction_coefficients: np.ndarray,
 ) -> np.ndarray:
+    """Calculate absorption coefficient
+
+    Args:
+        water_frac (np.ndarray): Water fraction
+        hhb (np.ndarray): Deoxy Hb conc
+        hbo2 (np.ndarray): Oxy Hb conc
+        extinction_coefficients (np.ndarray): Extinction coefficients
+
+    Returns:
+        np.ndarray: Array of mu_a for each wavelength and timepoint
+    """
     mua = np.outer(water_frac, extinction_coefficients[:, 3]) + np.log(10) * (
         np.outer(hhb, extinction_coefficients[:, 1])
         + np.outer(hbo2, extinction_coefficients[:, 2])
@@ -20,6 +31,16 @@ def calc_mua(
 def calc_mus(
     a: np.ndarray, b: np.ndarray, wavelengths: np.ndarray
 ) -> np.ndarray:
+    """Calculate scattering coefficient
+
+    Args:
+        a (np.ndarray): parameter a in scattering coefficient calculation
+        b (np.ndarray): parameter b in scattering coefficient calculation
+        wavelengths (np.ndarray): Wavelengths to calculate at
+
+    Returns:
+        np.ndarray: Array of mu_s for each wavelength and timepoint
+    """
     mus = np.power(
         np.outer(a, (wavelengths * 0.001)).T,
         -b,
